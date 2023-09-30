acd451c0434c003b67769a9d4c07ba87157c2d0e
Sam <sam.wheat@outlook.com>
2023-09-30 10:57:26 -0700

    Add DbContextOptions

---
+++ Vyntix.Fred.StagingDb/DbContextOptions_MSSQL.cs
@@ -0,0 +1,13 @@
+﻿namespace LeaderAnalytics.Vyntix.Fred.StagingDb;
+
+public class DbContextOptions_MSSQL : IDbContextOptions
+{
+    public DbContextOptions Options { get; set; }
+
+    public DbContextOptions_MSSQL(string connectionString)
+    {
+        DbContextOptionsBuilder builder = new DbContextOptionsBuilder();
+        builder.UseSqlServer(connectionString);
+        Options = builder.Options;
+    }
+}
---
+++ Vyntix.Fred.StagingDb/DbContextOptions_MySQL.cs
@@ -0,0 +1,13 @@
+﻿namespace LeaderAnalytics.Vyntix.Fred.StagingDb;
+
+public class DbContextOptions_MySQL : IDbContextOptions
+{
+    public DbContextOptions Options { get; set; }
+
+    public DbContextOptions_MySQL(string connectionString)
+    {
+        DbContextOptionsBuilder builder = new DbContextOptionsBuilder();
+        builder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        Options = builder.Options;
+    }
+}
